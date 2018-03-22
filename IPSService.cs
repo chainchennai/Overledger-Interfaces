43e96b14d68cd0ca0f3b9abda6e4445fed6fa32c
quantnetworkgv <gilbert.verdian@quant.network>
2018-03-22 20:54:52 +0000

    first commit

---
+++ IPSService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PS.Data;
+using PS.Data.PS.Data;
+
+namespace PS.Interfaces
+{
+    public interface IPSService
+    {
+        void SendSnapshot(PSSnapshot snapshot);
+    }
+}
---
+++ ISnapshotCollector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PS.Data;
+using PS.Data.PS.DAO;
+
+
+namespace PS.Interfaces
+{
+    public interface ISnapshotCollector 
+    {
+
+        PSSnapshot CollectSnapshot(SnapshotType snapshotType, bool includeComponentFileDetails);
+        
+			 
+    }
+}
